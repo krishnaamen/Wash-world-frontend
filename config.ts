14673f2f100a16581f28851c285ffc7ae7560c07
Krishna Prasad Khanal <krishnaamen@gmail.com>
2024-05-20 07:20:18 +0200

    changes in url

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
-export const  BASE_URL = 'https://5aed-80-208-69-75.ngrok-free.app'
+export const BASE_URL = 'https://12bc-80-208-69-75.ngrok-free.app'
 
-export enum CategoryEnum{
-    food='food',
-    transport="transport",
-    entertainment="entertainment",
-    bills="bills",
-    others="others"
+export enum CategoryEnum {
+    food = 'food',
+    transport = "transport",
+    entertainment = "entertainment",
+    bills = "bills",
+    others = "others"
 }
